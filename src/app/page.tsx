9233e4fbc9fac1c5b1d37cb9983ab7ca2496f498
Renan <renan.fachin@hotmail.com>
2023-05-16 13:50:15 -0300

    Configurações iniciais

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <h1>Hello world</h1>
+    <div className="m-10 flex h-20 w-20 items-center justify-center rounded bg-red-500 text-2xl font-bold text-white">
+      Hello
+    </div>
   )
 }

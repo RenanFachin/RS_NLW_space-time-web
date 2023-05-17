96a100412cd71d5047f521c97761549ff4a0c212
Renan <renan.fachin@hotmail.com>
2023-05-17 10:44:14 -0300

    Configurações inciais - Guide de cores, favicon e etc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
     <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
-      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
-      <h1 className="font-alt text-4xl font-bold">Sua cápsula do tempo</h1>
+      
     </div>
   )
 }

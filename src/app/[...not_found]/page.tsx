dc2fca1182170b87791e444154383c3d5f439f22
Renan <renan.fachin@hotmail.com>
2023-05-18 20:29:57 -0300

    Cria página de not found

---
+++ src/app/[...not_found]/page.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <span className="text-2xl">404</span>
+      <strong>Page not found</strong>
+    </div>
+  )
+}

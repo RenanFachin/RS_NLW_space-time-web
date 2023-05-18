95f967cb83dabcd5897409649225ebd6e428d633
Renan <renan.fachin@hotmail.com>
2023-05-18 19:55:51 -0300

    Cria página de novo registro de memória

--- src/app/memories/new/page.tsx
+++ src/app/memories/new/page.tsx
@@ -1,3 +1,51 @@
+import { Camera, ChevronLeft } from 'lucide-react'
+import Link from 'next/link'
+
 export default function NewMemory() {
-  return <p>new</p>
+  return (
+    <div className="flex flex-1 flex-col gap-4">
+      <Link
+        href="/"
+        className="flex items-center gap-1 text-sm text-gray-200 hover:text-gray-100"
+      >
+        <ChevronLeft className="h-4 w-4" />
+        Voltar à timeline
+      </Link>
+
+      <form className="flex flex-1 flex-col gap-2">
+        <div className="flex items-center gap-4">
+          <label
+            htmlFor="media"
+            className="flex cursor-pointer items-center gap-1.5 text-sm text-gray-200 hover:text-gray-100"
+          >
+            <Camera className="h-5 w-5" />
+            Anexar mídia
+          </label>
+
+          <label
+            htmlFor="isPublic"
+            className="flex items-center gap-1.5 text-sm text-gray-200 hover:text-gray-100"
+          >
+            <input
+              type="checkbox"
+              name="isPublic"
+              id="isPublic"
+              value="true"
+              className="h-4 w-4 rounded border-gray-400 bg-gray-700 text-purple-500"
+            />
+            Tornar memória pública
+          </label>
+        </div>
+        {/* invisible h-0 w-0 => faz o elemento ficar invisível e não ocupar espaço */}
+        <input type="file" id="media" className="invisible h-0 w-0" />
+
+        <textarea
+          name="content"
+          spellCheck={false}
+          className="w-full flex-1 resize-none rounded border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100 placeholder:text-gray-400 focus:ring-purple-300"
+          placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
+        />
+      </form>
+    </div>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -93,5 +93,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
